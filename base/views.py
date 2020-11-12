4c8c87b1255a744beb5fe7958a73249f5828538c
Ishita Gupta <ishita2kbti@gmail.com>
2020-11-12 17:25:19 +0530

    home and about page

--- base/views.py
+++ base/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse('<h1>home</h1>')
+	return render(request, 'base/home.html')
 
 def about(request):
-	return HttpResponse('<h1>about</h1>')
+	return render(request, 'base/about.html')
 
 def login(request):
 	return HttpResponse('<h1>login</h1>')
